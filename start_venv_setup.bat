9b36d057742422ba61c33990c1cd67cf9f884963
Joe Gaffney <jdgaffney36@gmail.com>
2025-01-12 22:25:50 +0000

    feeature: using cuda whl

--- start_venv_setup.bat
+++ start_venv_setup.bat
@@ -17,8 +17,11 @@ IF "%VIRTUAL_ENV%"=="" (
 echo Upgrading pip...
 python.exe -m pip install --upgrade pip
 
+echo Installing PyTorch with CUDA support...
+pip install torch==2.5.1+cu124 torchvision==0.20.1+cu124 torchaudio==2.5.1+cu124 --index-url https://download.pytorch.org/whl/cu124
+
 echo Installing requirements...
-pip install -r requirements.txt
+pip install -r requirements.txt 
 
 echo Virtual environment activated and requirements installed.
 CALL venv\Scripts\activate
