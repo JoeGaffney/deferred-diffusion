80f8c002dd08e4bcfb1024634940852e893a4d0c
Joe Gaffney <jdgaffney36@gmail.com>
2025-04-29 22:48:50 +0100

    wip: hunyuan quant

--- api/videos/models/hunyuan_video.py
+++ api/videos/models/hunyuan_video.py
@@ -8,29 +8,50 @@ from diffusers import (
 )
 from diffusers.utils import load_image
 from huggingface_hub import hf_hub_download
+from transformers import BitsAndBytesConfig, CLIPTextModel, LlamaModel, QuantoConfig
 
 from common.logger import logger
 from utils.utils import get_16_9_resolution
 from videos.context import VideoContext
 from videos.schemas import VideoRequest
 
+# quant_config = QuantoConfig(weights="int8")
+# quant_config = BitsAndBytesConfig(load_in_8bit=True)
+
+# quant_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.bfloat16)
+# quant_config = BitsAndBytesConfig(load_in_8bit=True, bnb_8bit_compute_dtype=torch.float16)
+quant_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_quant_type="fp4", bnb_4bit_compute_dtype=torch.bfloat16)
+
 
 @lru_cache(maxsize=1)
 def get_pipeline(model_id="hunyuanvideo-community/HunyuanVideo-I2V"):
-    # transformer = HunyuanVideoTransformer3DModel.from_pretrained(
-    #     model_id, subfolder="transformer", torch_dtype=torch.bfloat16
-    # )
-    gguf_transformer_path = hf_hub_download(
-        repo_id="city96/HunyuanVideo-I2V-gguf", filename="hunyuan-video-i2v-720p-Q5_K_M.gguf"
-    )
-    transformer = HunyuanVideoTransformer3DModel.from_single_file(
-        gguf_transformer_path,
-        quantization_config=GGUFQuantizationConfig(compute_dtype=torch.float16),
+    transformer = HunyuanVideoTransformer3DModel.from_pretrained(
+        model_id,
+        subfolder="transformer",
+        quantization_config=quant_config,
         torch_dtype=torch.bfloat16,
     )
+    # text_encoder = LlamaModel.from_pretrained(
+    #     model_id, subfolder="text_encoder", quantization_config=quant_config, torch_dtype=torch.float16
+    # )
+    # text_encoder_2 = CLIPTextModel.from_pretrained(
+    #     model_id, subfolder="text_encoder_2", quantization_config=quant_config, torch_dtype=torch.float16
+    # )
+    # gguf_transformer_path = hf_hub_download(
+    #     repo_id="city96/HunyuanVideo-I2V-gguf", filename="hunyuan-video-i2v-720p-Q5_K_M.gguf"
+    # )
+    # transformer = HunyuanVideoTransformer3DModel.from_single_file(
+    #     gguf_transformer_path,
+    #     quantization_config=GGUFQuantizationConfig(compute_dtype=torch.float16),
+    #     torch_dtype=torch.float16,
+    # )
 
     pipe = HunyuanVideoImageToVideoPipeline.from_pretrained(
-        model_id, transformer=transformer, torch_dtype=torch.float16
+        model_id,
+        transformer=transformer,
+        # text_encoder=text_encoder,
+        # text_encoder_2=text_encoder_2,
+        torch_dtype=torch.float16,
     )
 
     pipe.vae.enable_tiling()
@@ -49,7 +70,7 @@ def image_to_video(context: VideoContext):
     image = load_image(
         "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/guitar-man.png"
     )
-    image.resize((1280, 720))
+    image.resize((int(1280 / 2), int(720 / 2)))
 
     # video = pipe.__call__(
     #     width=image.size[0],
@@ -96,13 +117,13 @@ if __name__ == "__main__":
             VideoRequest(
                 model="hunyuanvideo-community/HunyuanVideo-I2V",
                 input_image_path="../tmp/tornado_v001.jpg",
-                output_video_path="../tmp/output/ltx_video.mp4",
+                output_video_path="../tmp/output/HunyuanVideo-I2V.mp4",
                 prompt="Tornado spinning in the sky, grass field moving in the wind, camera slowly zooming out",
-                num_inference_steps=35,
+                num_inference_steps=5,
                 guidance_scale=1,
                 max_width=width,
                 max_height=height,
-                num_frames=96,
+                num_frames=24,
             )
         )
     )
