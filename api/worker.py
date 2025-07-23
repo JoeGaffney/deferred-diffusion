b4c48b80c5286a9a4ff9bbc6ac966ff4e37a3db3
joe.gaffney <jdgaffney36@gmail.com>
2025-07-23 23:31:27 +0100

    fix: updated some configs and it tests

--- api/worker.py
+++ api/worker.py
@@ -1,8 +1,17 @@
 import os
-import time
 
 from celery import Celery
 
 celery_app = Celery(
     "deferred-diffusion", broker=os.getenv("CELERY_BROKER_URL"), backend=os.getenv("CELERY_RESULT_BACKEND")
+)
+celery_app.conf.broker_transport_options = {
+    "socket_timeout": 15,  # Timeout for Redis socket ops (in seconds)
+    "retry_on_timeout": True,  # Retry on timeout errors
+    "max_retries": 2,  # Number of retries for send_task() delivery
+}
+
+celery_app.conf.update(
+    result_backend_always_retry=False,  # Do not always retry result backend operations
+    result_backend_max_retries=2,  # Number of retries for result backend operations
 )
