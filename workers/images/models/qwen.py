6e06506872e71ef06e636c153d8c372b3faa200d
joe.gaffney <jdgaffney36@gmail.com>
2025-10-12 19:15:31 +0100

    feature: added nunchaku to qwen pipline and updated to multi input edit model

--- workers/images/models/qwen.py
+++ workers/images/models/qwen.py
@@ -3,12 +3,14 @@ import math
 import torch
 from diffusers import (
     FlowMatchEulerDiscreteScheduler,
-    QwenImageEditPipeline,
+    QwenImageEditPlusPipeline,
     QwenImageImg2ImgPipeline,
     QwenImageInpaintPipeline,
     QwenImagePipeline,
     QwenImageTransformer2DModel,
 )
+from nunchaku import NunchakuQwenImageTransformer2DModel
+from nunchaku.utils import get_gpu_memory, get_precision
 from PIL import Image
 
 from common.config import IMAGE_CPU_OFFLOAD, IMAGE_TRANSFORMER_PRECISION
@@ -21,21 +23,11 @@ from common.pipeline_helpers import (
 from common.text_encoders import qwen_encode
 from images.context import ImageContext
 
+_use_nunchaku = False
 
-@decorator_global_pipeline_cache
-def get_pipeline(model_id) -> QwenImagePipeline:
-    args = {}
 
-    args["transformer"] = get_quantized_model(
-        model_id=model_id,
-        subfolder="transformer",
-        model_class=QwenImageTransformer2DModel,
-        target_precision=16,
-        torch_dtype=torch.bfloat16,
-    )
-
-    # From
-    # https://github.com/ModelTC/Qwen-Image-Lightning/blob/342260e8f5468d2f24d084ce04f55e101007118b/generate_with_diffusers.py#L82C9-L97C10
+def get_scheduler():
+    # From https://github.com/ModelTC/Qwen-Image-Lightning/blob/342260e8f5468d2f24d084ce04f55e101007118b/generate_with_diffusers.py#L82C9-L97C10
     scheduler_config = {
         "base_image_seq_len": 256,
         "base_shift": math.log(3),  # We use shift=3 in distillation
@@ -52,70 +44,78 @@ def get_pipeline(model_id) -> QwenImagePipeline:
         "use_exponential_sigmas": False,
         "use_karras_sigmas": False,
     }
-    args["scheduler"] = FlowMatchEulerDiscreteScheduler.from_config(scheduler_config)
+    return FlowMatchEulerDiscreteScheduler.from_config(scheduler_config)
+
+
+@decorator_global_pipeline_cache
+def get_pipeline(model_id) -> QwenImagePipeline:
+    if _use_nunchaku:
+        rank = 128  # you can also use the rank=128 model to improve the quality
+        model_paths = {
+            4: f"nunchaku-tech/nunchaku-qwen-image/svdq-{get_precision()}_r{rank}-qwen-image-lightningv1.0-4steps.safetensors",
+            8: f"nunchaku-tech/nunchaku-qwen-image/svdq-{get_precision()}_r{rank}-qwen-image-lightningv1.1-8steps.safetensors",
+        }
+        transformer = NunchakuQwenImageTransformer2DModel.from_pretrained(model_paths[8])
+    else:
+        transformer = get_quantized_model(
+            model_id="ovedrive/qwen-image-4bit",
+            subfolder="transformer",
+            model_class=QwenImageTransformer2DModel,
+            target_precision=16,
+            torch_dtype=torch.bfloat16,
+        )
 
     pipe = QwenImagePipeline.from_pretrained(
         model_id,
         text_encoder=None,
         tokenizer=None,
+        scheduler=get_scheduler(),
+        transformer=transformer,
         torch_dtype=torch.bfloat16,
-        **args,
     )
-    pipe.load_lora_weights(
-        "lightx2v/Qwen-Image-Lightning", weight_name="Qwen-Image-Lightning-8steps-V1.1-bf16.safetensors"
-    )
+    if not _use_nunchaku:
+        pipe.load_lora_weights(
+            "lightx2v/Qwen-Image-Lightning", weight_name="Qwen-Image-Lightning-8steps-V2.0-bf16.safetensors"
+        )
+
     return optimize_pipeline(pipe, offload=IMAGE_CPU_OFFLOAD)
 
 
 @decorator_global_pipeline_cache
-def get_edit_pipeline(model_id) -> QwenImageEditPipeline:
-    args = {}
+def get_edit_pipeline(model_id) -> QwenImageEditPlusPipeline:
+    if _use_nunchaku:
+        num_inference_steps = 8  # you can also use the 8-step model to improve the quality
+        rank = 128  # you can also use the rank=128 model to improve the quality
+        model_path = f"nunchaku-tech/nunchaku-qwen-image-edit-2509/svdq-{get_precision()}_r{rank}-qwen-image-edit-2509-lightningv2.0-{num_inference_steps}steps.safetensors"
+        transformer = NunchakuQwenImageTransformer2DModel.from_pretrained(model_path)
+    else:
+        transformer = get_quantized_model(
+            model_id="ovedrive/Qwen-Image-Edit-2509-4bit",
+            subfolder="transformer",
+            model_class=QwenImageTransformer2DModel,
+            target_precision=16,
+            torch_dtype=torch.bfloat16,
+        )
 
-    args["transformer"] = get_quantized_model(
-        model_id=model_id,
-        subfolder="transformer",
-        model_class=QwenImageTransformer2DModel,
-        target_precision=16,
-        torch_dtype=torch.bfloat16,
-    )
-
-    # From
-    # https://github.com/ModelTC/Qwen-Image-Lightning/blob/342260e8f5468d2f24d084ce04f55e101007118b/generate_with_diffusers.py#L82C9-L97C10
-    scheduler_config = {
-        "base_image_seq_len": 256,
-        "base_shift": math.log(3),  # We use shift=3 in distillation
-        "invert_sigmas": False,
-        "max_image_seq_len": 8192,
-        "max_shift": math.log(3),  # We use shift=3 in distillation
-        "num_train_timesteps": 1000,
-        "shift": 1.0,
-        "shift_terminal": None,  # set shift_terminal to None
-        "stochastic_sampling": False,
-        "time_shift_type": "exponential",
-        "use_beta_sigmas": False,
-        "use_dynamic_shifting": True,
-        "use_exponential_sigmas": False,
-        "use_karras_sigmas": False,
-    }
-    args["scheduler"] = FlowMatchEulerDiscreteScheduler.from_config(scheduler_config)
-
-    pipe = QwenImageEditPipeline.from_pretrained(
-        "Qwen/Qwen-Image-Edit",
+    pipe = QwenImageEditPlusPipeline.from_pretrained(
+        model_id,
         text_encoder=None,
         tokenizer=None,
+        transformer=transformer,
+        scheduler=get_scheduler(),
         torch_dtype=torch.bfloat16,
-        **args,
     )
-    pipe.load_lora_weights(
-        "lightx2v/Qwen-Image-Lightning", weight_name="Qwen-Image-Lightning-8steps-V1.1-bf16.safetensors"
-    )
+    if not _use_nunchaku:
+        pipe.load_lora_weights(
+            "lightx2v/Qwen-Image-Lightning", weight_name="Qwen-Image-Edit-Lightning-8steps-V1.0-bf16.safetensors"
+        )
+
     return optimize_pipeline(pipe, offload=IMAGE_CPU_OFFLOAD)
 
 
 def text_to_image_call(context: ImageContext):
-    prompt_embeds, prompt_embeds_mask = qwen_encode(context.data.prompt + " Ultra HD, 4K, cinematic composition.")
-
-    pipe = get_pipeline("ovedrive/qwen-image-4bit")
+    prompt_embeds, prompt_embeds_mask = qwen_encode(context.data.prompt + "Ultra HD, 4K, cinematic composition.")
+    pipe = get_pipeline("Qwen/Qwen-Image-Edit-2509")
 
     args = {
         "width": context.width,
@@ -135,9 +135,9 @@ def text_to_image_call(context: ImageContext):
 
 
 def image_edit_call(context: ImageContext):
-    prompt_embeds, prompt_embeds_mask = qwen_encode(context.data.prompt + " Ultra HD, 4K, cinematic composition.")
+    prompt_embeds, prompt_embeds_mask = qwen_encode(context.data.prompt + "Ultra HD, 4K, cinematic composition.")
 
-    pipe = get_edit_pipeline("ovedrive/qwen-image-edit-4bit")
+    pipe = get_edit_pipeline("ovedrive/Qwen-Image-Edit-2509-4bit")
 
     args = {
         "width": context.width,
@@ -158,7 +158,7 @@ def image_edit_call(context: ImageContext):
 
 
 def inpainting_call(context: ImageContext):
-    prompt_embeds, prompt_embeds_mask = qwen_encode(context.data.prompt + " Ultra HD, 4K, cinematic composition.")
+    prompt_embeds, prompt_embeds_mask = qwen_encode(context.data.prompt + "Ultra HD, 4K, cinematic composition.")
 
     pipe = QwenImageInpaintPipeline.from_pipe(get_pipeline("ovedrive/qwen-image-4bit"))
 
--- workers/tests/images/test_qwen_image.py
+++ workers/tests/images/test_qwen_image.py
@@ -3,12 +3,14 @@ from typing import List, Literal
 import pytest
 from helpers import (
     image_to_image,
+    image_to_image_alt,
     inpainting,
     inpainting_alt,
     references_canny,
     references_face,
     references_style,
     text_to_image,
+    text_to_image_alt,
 )
 
 from images.schemas import ModelName
@@ -16,9 +18,14 @@ from images.schemas import ModelName
 models: List[ModelName] = ["qwen-image"]
 
 
-# @pytest.mark.parametrize("model", models)
-# def test_text_to_image(model):
-#     text_to_image(model)
+@pytest.mark.parametrize("model", models)
+def test_text_to_image(model):
+    text_to_image(model)
+
+
+@pytest.mark.parametrize("model", models)
+def test_text_to_image_alt(model):
+    text_to_image_alt(model)
 
 
 # @pytest.mark.parametrize("model", models)
@@ -29,3 +36,8 @@ models: List[ModelName] = ["qwen-image"]
 @pytest.mark.parametrize("model", models)
 def test_image_to_image(model):
     image_to_image(model)
+
+
+@pytest.mark.parametrize("model", models)
+def test_image_to_image_alt(model):
+    image_to_image_alt(model)
